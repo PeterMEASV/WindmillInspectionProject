77b481fc9819e1cf9e94f1b725181d5fd924e698
Peteris Meiris <petmei01@easv365.dk>
2026-03-05 14:38:24 +0100

    MQTT template for other members

---
+++ Server/Api/Controllers/M2CMqttController.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+using Mqtt.Controllers;
+
+namespace Api.Controllers;
+
+public class M2CMqttController(ILogger<M2CMqttController> logger) : MqttController
+{
+    [MqttRoute("farm/Mindst2Commits/windmill/{turbineId}/telemetry")]
+    public async Task SubscribeToWindmillTelemetry(object data, string turbineId)
+    {
+        logger.LogInformation("Data from the turbine: " + turbineId);
+        logger.LogInformation(JsonSerializer.Serialize(data));
+        
+    }
+    
+    
+}
--- Server/Api/Program.cs
+++ Server/Api/Program.cs
@@ -1,8 +1,10 @@
 using api;
 using DataAccess;
 using Microsoft.EntityFrameworkCore;
+using Mqtt.Controllers;
 
 var builder = WebApplication.CreateBuilder(args);
+
 var appOptions = builder.Services.AddAppOptions(builder.Configuration);
 
 builder.Services.AddCors();
@@ -10,6 +12,7 @@ builder.Services.AddDbContext<MyDbContext>(conf =>
 {
     conf.UseNpgsql(appOptions.DBConnectionString);
 });
+builder.Services.AddMqttControllers();
 builder.Services.AddControllers();
 builder.Services.AddOpenApiDocument();
 
@@ -20,6 +23,9 @@ app.MapControllers();
 app.UseOpenApi();
 app.UseSwaggerUi();
 await app.GenerateApiClientsFromOpenApi("/../../client/src/generated-ts-client.ts");
+
+var mqtt = app.Services.GetRequiredService<IMqttClientService>();
+await mqtt.ConnectAsync("broker.hivemq.com", 1883);
 
 
 app.Run();
